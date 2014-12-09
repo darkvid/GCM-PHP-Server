637b26310286f09f564131bbd41e5007ca25c8e4
darkvid <David Galisteo>
2014-12-09 12:18:40 +0100

    index con input para mensaje

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Envio comunicaciones GCM</title>
+</head>
+<body>
+	<h1>
+		Envío de comunicaciones GCM
+	</h1>
+
+	<form action="./envio.php">
+		<label for="message">Introduce el mensaje</label>
+		<input type="text" name="message">
+		<input type="submit" value="Enviar">
+	</form>
+
+</body>
+</html>
